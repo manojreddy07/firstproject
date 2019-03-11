ede00ccc7515a8c295079ab6b86a47b096420b0c
nimma <nimma@192.168.0.145>
2019-03-10 21:13:10 -0500

    3 statements

--- demo/src/main/java/com/example/demo/hello.java
+++ demo/src/main/java/com/example/demo/hello.java
@@ -4,6 +4,9 @@ public class hello {
  
 	public static void main(String[] args) {
        System.out.println("hello");
+       System.out.println("hello world");
+       System.out.println("hello world java");
+
        
 }
 }
